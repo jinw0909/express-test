6a35c86779d69d641a56733f0e7b9fa1bc675916
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-05-02 13:31:31 +0900

    2400502 commit

--- routes/puppet.js
+++ routes/puppet.js
@@ -18,7 +18,10 @@ async function clickLoadMore(page, selector) {
 }
 async function run() {
     // Launch the browser and open a new page
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     // await page.setViewport({width: 1920, height: 1080});
     //Go to the target web page
