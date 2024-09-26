00792f7455290656c4e1065c04e6a88ea4acd88a
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-09-26 18:05:12 +0900

    adjust height

--- routes/capture.js
+++ routes/capture.js
@@ -395,7 +395,7 @@ const capturePremium = async function(req, res) {
             // Set a higher-resolution viewport and device scale factor
             await page.setViewport({
                 width: 760,        // Set the width of the viewport
-                height: 1080,       // Set the height of the viewport
+                height: 540,       // Set the height of the viewport
                 deviceScaleFactor: 2 // Set to 2 for higher resolution (simulating a retina display)
             });
 
