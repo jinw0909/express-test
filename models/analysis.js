223163368d1c8f8624e53ba99012a05537a7cf12
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-10-02 16:27:09 +0900

    altered orm table names

--- models/analysis.js
+++ models/analysis.js
@@ -1,62 +1,12 @@
-// const { DataTypes } = require('sequelize');
-// const sequelize = require('../sequelize');
-//
-// const Analysis = sequelize.define('Analysis', {
-//     id : {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true
-//     },
-//     //Define model attributes
-//     createdAt: {
-//         type: DataTypes.DATE,
-//         allowNull: false
-//     },
-//     summary: {
-//         type: DataTypes.TEXT,
-//         allowNull: false
-//     },
-//     date: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     imageUrl : {
-//         type:  DataTypes.STRING,
-//         allowNull: true
-//     },
-//     publisher: {
-//         type: DataTypes.STRING,
-//         allowNull: true
-//     },
-//     analysis: {
-//         type: DataTypes.TEXT,
-//         allowNull: false
-//     },
-//     updatedAt: {
-//         type: DataTypes.DATE,
-//         allowNull: false
-//     },
-//     ref: {
-//         type: DataTypes.STRING,
-//         allowNull: true,
-//         references: {
-//             model: 'Viewpoints', // This is the table name of Viewpoint
-//             key: 'id'
-//         }
-//     }
-// })
-//
-// module.exports = Analysis;
-
-
 const { DataTypes } = require('sequelize');
 const sequelize = require('../sequelize');
 
 const Analysis = sequelize.define('Analysis', {
-    id: {
+    id : {
         type: DataTypes.INTEGER,
         primaryKey: true
     },
-    // Define model attributes
+    //Define model attributes
     createdAt: {
         type: DataTypes.DATE,
         allowNull: false
@@ -69,8 +19,8 @@ const Analysis = sequelize.define('Analysis', {
         type: DataTypes.STRING,
         allowNull: true
     },
-    imageUrl: {
-        type: DataTypes.STRING,
+    imageUrl : {
+        type:  DataTypes.STRING,
         allowNull: true
     },
     publisher: {
@@ -93,9 +43,6 @@ const Analysis = sequelize.define('Analysis', {
             key: 'id'
         }
     }
-}, {
-    tableName: 'news_en', // Explicitly set the table name
-    timestamps: true      // Enable createdAt and updatedAt by default
-});
+})
 
 module.exports = Analysis;
